8b151d407dc391ea339a82927648647ce3c3cbf8
Joe <joecolasurdo@eltoro.com>
2018-05-29 15:07:41 -0700

    Updated docs.

--- drygopher/cmd/root.go
+++ drygopher/cmd/root.go
@@ -38,6 +38,10 @@ Run coverage analysis, excluding vendor and test packages, and suppress the gene
 
   $ drygopher -d --suppressprofile
 
+Run coverage analysis, excluding default packages (vendor and test), and view the resulting coverage heatmap. Note that ';' is used rather than '&&' between the commands to ensure that 'go tool' is run even if drygopher complains that coverage is below standard.
+
+  $ drygopher -d; go tool cover -html=coverage.out
+
 Run coverage analysis, excluding vendor and test packages, and also exclude any packages whose name ends with "service". Note that in this case, we enclose the expression in single quotes to prevent globbing.
 
   $drygopher -d -e 'service$'
@@ -57,8 +61,24 @@ The following commands are all equivalent:
   Using defaults and a single expression:
   $drygopher -d -e "'cmd$|iface$|mock'"
 
-Note that when supplying a list of expressions for -e, the list must be comma delimited. As such, literal commas cannot be used when supplying a list of expressions for the -e flag. Generally, this shouldn't be an issue since commas are not typically valid in package names.
-`
+  Note that when supplying a list of expressions for -e, the list must be comma delimited. As such, literal commas cannot be used when supplying a list of expressions for the -e flag. Generally, this shouldn't be an issue since commas are not typically valid in package names.
+
+Use the 'coveragepct' file, which contains the coverage percentage calculated by drygopher, as a parameter in some other command. This example shows a trivial 'echo' command that returns 'Coverage: 100%'.
+
+  $ drygopher -d; echo Coverage: $(cat coveragepct)%
+
+Notes:
+
+ - When drygopher encounters a package that has no associated unit tests, it creates an estimate for the number of statements that the package might contain. This estimate is the average (median) number of statements found across packages that do have coverage. In such cases, drygopher does not go any further than making an estimate, as it does not want to make assumptions about the code authors' intent. By the same token, it would be generally incorrect to presume that the package contained no statements at all. This is merely a means to an end until the author decides to either a) cover the package with unit tests, or b) exclude the package from coverage analysis entirely.
+
+ - drygopher will exit with a non-zero status code (and display an informational error message) if the calculated coverage falls below the coverage standard. To override this behavior, either increase code coverage, or reduce the coverage standard (using the -s flag).
+ 
+ - drygopher will exit with a non-zero status code (and display an informational error message) if any unit tests fail during analysis. Unit test failures have a higher priority than coverage failures. Thus, if a system under test fails for both coverage and unit test failure, drygopher will report the unit test failure rather than the coverage failure.
+
+ - drygopher will exit with a non-zero status code and little other information if build errors are encountered while testing.
+ 
+ - When drygopher tests each package, it applies the -race flag. This decreases testing performance, but ensures that applicable race conditions are captured during analysis.
+ `
 
 var rootCmd = &cobra.Command{
 	Use:   "drygopher [flags]",
